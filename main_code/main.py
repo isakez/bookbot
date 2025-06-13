13b958b338fb9e753da7bc900f59bea7aec0a7ba
isakez <isaksanchez21@gmail.com>
2025-06-13 07:43:25 +0200

    version con las principales funcionalidades

--- main_code/main.py
+++ main_code/main.py
@@ -2,15 +2,27 @@
 
 import requests
 from stats_book import *
+import sys
 
-relative_path = '/home/isaac/proyectos/bookbot/books/frankenstein.txt'
+
 
 def main():
+ 
+ if len(sys.argv) != 2:
+        print("para un correcto funcionamiento usa dos argumentos python3 main.py <direccion  archivo de texto>")
+        sys.exit(1)
 
+ relative_path = sys.argv[1]
+        
  contenido = get_book_text(relative_path)
  num_words = get_words_text(contenido)
  print(f"{num_words} words found in the document")
  dict_char = get_char_text(contenido)
- print(dict_char)
+ 
+ dict_char_ordenado = sorted_dict(dict_char)
+ print(dict_char_ordenado)
+
+    
+
 main()
 
--- main_code/stats_book.py
+++ main_code/stats_book.py
@@ -25,7 +25,7 @@ def get_char_text(contenido):
     char_dict = {}
     lista_caracteres = contenido.lower()
     lista_caracteres = list(lista_caracteres)
-    count = 0
+
     for char in lista_caracteres:
         if char in char_dict:
             char_dict[char] += 1
@@ -33,4 +33,32 @@ def get_char_text(contenido):
             char_dict[char] = 1
 
     return char_dict
+
+def my_sort(dict):
+    return dict['num']
+
+def sorted_dict(char_dict):
+    llaves = list(char_dict.keys())
+    valores = list(char_dict.values())
+    lista_dict =[]
+    i=0 
+    for i in range(len(llaves)):
+        if llaves[i].isalpha():
+
+            lista_dict.append({"char":llaves[i],"num":valores[i]})
+    
+    lista_dict.sort(reverse = True, key=my_sort)
+
+    return lista_dict
+
+
+
+    
+
+
+
+
+
+
+
 
