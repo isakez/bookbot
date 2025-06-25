4be5151a1bdcb95d8b769ffab965b15b2aedd537
isakez <isaksanchez21@gmail.com>
2025-06-25 19:20:50 +0200

    change to show and automatization of the downloads from the gutenberg api, muy guay

--- main_code/gutenberg_api.py
+++ main_code/gutenberg_api.py
@@ -1,4 +1,5 @@
 import requests
+import sys
 
 url = 'https://gutendex.com/books/'
 
@@ -8,25 +9,56 @@ print(f"status code: {r.status_code}")
 
 response_dict = r.json()
 
-print(response_dict.keys())
+# print(response_dict.keys())
 
 book_dict = response_dict['results']
-print(book_dict[0].keys())
-print(book_dict[0]['formats']['text/plain; charset=us-ascii'])
 
-book_url = book_dict[0]['formats']['text/plain; charset=us-ascii']
+def show_results(page):
+  i=0
+  for i in range(11*(page-1),11*page):
 
-book_response = requests.get(book_url)
+    print(f'{i}: {book_dict[i]['title']}')
 
-if book_response.status_code == 200:
+  return None
 
-    with open("frankestein.txt", "w",encoding='utf-8') as f:
-        f.write(book_response.text)
-    print("archivo guardado como frankstein.txt")
-else:
-    print(f"error en la request. status code: {book_response.status_code}")
+user_input = None
+page = 1
+
+print('just say yes/next/exit')
+
+while user_input != 'exit':
 
 
+  show_results(page)  
+
+  print('do you want to download?')
+  user_input = input().lower()
+
+  if user_input == 'yes':
+
+    print('wchich number?')
+    book_number = int(input())
+
+    book_url = book_dict[book_number]['formats']['text/plain; charset=us-ascii']
+    book_title = book_dict[book_number]['title']
+    book_response = requests.get(book_url)
+    file_path= f"/home/isaac/proyectos/bookbot/books/{book_title}.txt"
+
+    if book_response.status_code == 200:
+
+      with open(file_path, "w",encoding='utf-8') as f:
+          f.write(book_response.text)
+      print(f"archivo guardado como {file_path}")
+    else:
+      print(f"error en la request. status code: {book_response.status_code}")
+
+  if user_input == 'next':
+     page += 1
+     
+
+  if user_input == 'exit':
+    sys.exit()
+    
 
 
 
